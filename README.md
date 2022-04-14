98c41d1c5294331cf911a23135613eb1eca59c10
Kevin Mao <hello@kmao.dev>
2022-04-14 14:55:39 +1000

    hardhat installation & etherscan verification and openzepplin installation

---
+++ README.md
@@ -0,0 +1,30 @@
+# Basic Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
+
+make sure to run these commands after installation
+
+```shell
+npm install --save-dev @nomiclabs/hardhat-etherscan
+```
+
+used for contract verification on etherscan
+
+```shell
+npm install --save-dev @openzeppelin/hardhat-upgrades
+npm install @openzeppelin/contracts
+```
+
+installs the openzepplin contracts
---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const RINKEBY_PRIVATE_KEY = "";
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/YzUO9BBO6O-DBXi27dTAD2fxdnsgfzpL",
+      accounts: [`${RINKEBY_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "RBTBZCNVW7I13Y7SHHA5WGRFMHKC6I8I6F",
+  },
+};
