516ded6164e317815b3d4aa1df5aa2234b4c3640
Kevin Mao <hello@kmao.dev>
2022-04-19 12:59:40 +1000

    fix priv key issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -23,15 +24,13 @@ require("@nomiclabs/hardhat-etherscan");
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const RINKEBY_PRIVATE_KEY =
-  "0x0ecb3f9ec4bbf5d13d1cf2aa57d7e54e237ca90bb9e2d49ec25355e9df433e83";
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
       url: "https://eth-rinkeby.alchemyapi.io/v2/YzUO9BBO6O-DBXi27dTAD2fxdnsgfzpL",
-      accounts: [`${RINKEBY_PRIVATE_KEY}`],
+      accounts: [`${PRIVATE_KEY}`],
     },
   },
   etherscan: {
