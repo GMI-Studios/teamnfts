ebd60b5ab05c255675e7dc7e736a62e64b1f0605
Kevin Mao <hello@kmao.dev>
2022-04-19 11:33:29 +1000

    add metadata functions

--- contracts/gminfts.sol
+++ contracts/gminfts.sol
@@ -1,33 +1,42 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+/// @title Custom 721s for GMIStudios
+/// @author @kmao37 @gmistudios 
+/// @notice NFTs are not meant to be traded & will be locked after minting
+/// @dev These 721s are not meant to be tradeable 
+contract GMITeamNFTs is ERC721, Ownable {
 
-contract GMITeamNFTs is IERC721 {
+    string public baseURI = "";
 
+    // Contract Creation
+    constructor()ERC721("GMI Team NFTs", "GMI") { 
+    }      
 
+    /// @notice Mints _amount number of NFTs to an array of addresses
+    /// @param _amount number of NFTs to mint
+    /// @param addressUser array of addresses to mint to    
+    function mintMultipleToUser(uint256 _amount, address[] addressUser) external onlyOwner {
+        for (uint i=0; i<addressUser.length; i++) {
+            _mint(addressUser[i], _amount);
+        }
+    }   
 
-    // // needs to be unlocked for the mint function to work
-    // bool locked = false;
+    function _baseURI() internal view virtual override returns (string memory) {
+		return baseURI;
+	}
 
-    // // Contract Creation
-    // constructor(string memory newBaseURI)  { 
-    //     _baseURI = newBaseURI;
-    // }      
+	function setBaseURI(string calldata _uri) external onlyOwner{
+		baseURI = _uri;
+	}
 
-    // function setBaseURI(string memory baseURI) external onlyOwner {
-    //     _baseURI = baseURI;
-    // }
+    // we don't actually need this, this is for more aesthetic purposes
+    function _startTokenId() internal view virtual override returns (uint256) {
+        return 1;
+    }
 
-    // /**
-    //  * @dev Mints _amount number of NFTs to an array of addresses, utilising erc721a for lower gas
-    //  * @param _amount number of NFTs to mint
-    //  * @param addressUser array of addresses to mint to    
-    //  */
-    // function mintMultipleToUser(uint256 _amount, address[] addressUser) external onlyOwner {
-    //     for (uint i=0; i<addressUser.length; 1++) {
-    //         _mint(addressUser[i], _amount);
-    //     }
-    // }   
+    
+
 }
